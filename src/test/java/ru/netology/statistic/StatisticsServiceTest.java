a9707129fd586524d47e504ab330a46f878e9e38
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-05-27 19:21:34 +0300

    added counter instruction

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,8 +9,7 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-        //assertEquals(12, service.findMax(new long[]{12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}));
-        //assertEquals(12, service.findMax(new long[]{11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}));
+
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
