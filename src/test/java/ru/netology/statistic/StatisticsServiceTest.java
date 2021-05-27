ad9c3f0642968c65d0f953d8867af72e123b4675
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-05-27 19:21:13 +0300

    added counter instruction

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+        //assertEquals(12, service.findMax(new long[]{12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}));
+        //assertEquals(12, service.findMax(new long[]{11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}));
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
 }
